d4d32079866025329e142662401910c9295c6ab9
aizenandragon1-eng <aizenandragon1@gmail.com>
2026-01-14 19:55:43 +0200

    Update ModuleManager.java

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -9,9 +9,6 @@ import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.modules.client.ClickGui;
 import me.alpha432.oyvey.features.modules.client.HudEditor;
 import me.alpha432.oyvey.features.modules.client.Notifications;
-import me.alpha432.oyvey.features.modules.combat.Criticals;
-import me.alpha432.oyvey.features.modules.combat.AutoTotem;
-import me.alpha432.oyvey.features.modules.combat.AutoAnchor;
 import me.alpha432.oyvey.features.modules.hud.Coordinates;
 import me.alpha432.oyvey.features.modules.hud.Watermark;
 import me.alpha432.oyvey.features.modules.misc.MCF;
@@ -21,6 +18,8 @@ import me.alpha432.oyvey.features.modules.player.FastPlace;
 import me.alpha432.oyvey.features.modules.player.NoFall;
 import me.alpha432.oyvey.features.modules.player.Velocity;
 import me.alpha432.oyvey.features.modules.render.BlockHighlight;
+import me.alpha432.oyvey.features.modules.combat.AutoAnchor;
+import me.alpha432.oyvey.features.modules.combat.AutoTotem;
 import me.alpha432.oyvey.util.traits.Jsonable;
 import me.alpha432.oyvey.util.traits.Util;
 
@@ -32,30 +31,22 @@ public class ModuleManager implements Jsonable, Util {
     private final List<Module> modules = new ArrayList<>();
 
     public void init() {
-        // Client Modules
         register(new Watermark());
         register(new Coordinates());
         register(new HudEditor());
         register(new ClickGui());
         register(new Notifications());
-
-        // Combat Modules
-        register(new Criticals());
-        register(new AutoTotem());
-        register(new AutoAnchor());
         register(new MCF());
-
-        // Movement Modules
         register(new Step());
         register(new ReverseStep());
-
-        // Player Modules
         register(new FastPlace());
         register(new Velocity());
+        register(new BlockHighlight());
         register(new NoFall());
 
-        // Render Modules
-        register(new BlockHighlight());
+        // Combat modules
+        register(new AutoAnchor());
+        register(new AutoTotem());
     }
 
     public void register(Module module) {
